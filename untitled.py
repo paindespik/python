6e3dd764db94dc50f26df500b6ef7e9805dc6dc8
paindespik <seandriard3@gmail.com>
2019-07-02 18:23:43 +0200

    new commit

--- untitled.py
+++ untitled.py
@@ -1,8 +1,6 @@
 #!/usr/local/lib
 # -*-coding:Utf-8 -*
 
-chaine1= "beuleu"
-chaine2="julia"
-chaine3= 8
-
-print ("je sais {0} {1} pas {2}".format(chaine1,chaine2,chaine3))
+ma_liste=[3,7]
+ma_liste.append(56)
+print(ma_liste)
