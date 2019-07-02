4a8209ddbe22373a74f6b30d8fd7dde4c40168e6
paindespik <seandriard3@gmail.com>
2019-07-02 21:40:03 +0200

    new commit

--- roullette.py
+++ roullette.py
@@ -2,7 +2,7 @@
 # -*-coding:Utf-8 -*
 import random
 sommeArgent =input("quel est votre somme d'argent de départ:  ")
-numeroMise=0
+numeroMise=-1
 mise=0
 try:
 	sommeArgent=int(sommeArgent)
@@ -14,8 +14,8 @@ except AssertionError:
 
 while sommeArgent>0:
 
-	while numeroMise<0 || numeroMise>49:
-		numeroMise=input("Sur quel nupéro souhaiter vous miser? (entre 0 et 49)")
+	while numeroMise<0 or numeroMise>49:
+		numeroMise=input("Sur quel nupéro souhaiter vous miser? (entre 0 et 49): ")
 		try:
 			numeroMise=int(numeroMise)
 			assert numeroMise>=0 
@@ -25,26 +25,31 @@ while sommeArgent>0:
 		except ValueError:
 			print("vous devez rentrer un nombre")
 			numeroMise=-1
-	mise=input("Combien d'argent souhaiter vous miser?")
+
+
+	mise=input("Combien d'argent souhaiter vous miser?: ")
 	try:
 		mise=int(mise)
 		assert mise<=sommeArgent
 		assert mise>0
 	except :
 		print("vous devez rentrer un numéro")
+
+
 	rand=random.randint(0,50)
 	if rand==numeroMise:
 		print("BRAVO!!! vous êtes tombé sur votre numéro! Vous gagnez ",mise*3, "euros")
-		sommeArgent+=mise*3
+		sommeArgent+=mise*2
 
 	elif random.randint(0,2)==0:
 		print("vous etes tombés sur le numéro", rand)
-		print("vous n'êtes pas tombé sur le bon numéro, mais vous gardé la moitié de votre mise, car vous être tombé sur la bonne couleur")	
-		sommeArgent-=mise/2
+		print("vous n'êtes pas tombé sur le bon numéro, mais vous gardé votre mise, car vous être tombé sur la bonne couleur")	
 	else:
 		print("vous etes tombé sur le numéro", rand)
 		print("pas de chance, vous perdez votre mise")
 		sommeArgent-=mise
 	print("il vous reste", sommeArgent, "euros")
+
+	numeroMise=-1
 
 
