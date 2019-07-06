479aa43d54ae3f76441f4de54ed9774b3efb69bf
paindespik <seandriard3@gmail.com>
2019-07-07 00:53:20 +0200

    new commit

--- untitled.py
+++ untitled.py
@@ -1,13 +1,30 @@
 #!/usr/local/lib
 # -*-coding:Utf-8 -*
 
-def afficher_flottant(beuleu):
-	if type(beuleu)is not float:
-		raise TypeError("le paramètre doit être un float")
-	beuleu=str(beuleu)
-	partie_entiere, partie_flottante=beuleu.split(".")
-	beuleu2=",".join([partie_entiere,partie_flottante[:3]])
-	print(beuleu2)
+def afficher(*parametres, sep=' ', fin='\n'):
+    """Fonction chargée de reproduire le comportement de print.
+    
+    Elle doit finir par faire appel à print pour afficher le résultat.
+    Mais les paramètres devront déjà avoir été formatés. 
+    On doit passer à print une unique chaîne, en lui spécifiant de ne rien mettre à la fin :
 
-afficher_flottant(3.99999999)
+    print(chaine, end='')"""
+    
+    # Les paramètres sont sous la forme d'un tuple
+    # Or on a besoin de les convertir
+    # Mais on ne peut pas modifier un tuple
+    # On a plusieurs possibilités, ici je choisis de convertir le tuple en liste
+    parametres = list(parametres)
+    # On va commencer par convertir toutes les valeurs en chaîne
+    # Sinon on va avoir quelques problèmes lors du join
+    for i, parametre in enumerate(parametres):
+        parametres[i] = str(parametre)
+    # La liste des paramètres ne contient plus que des chaînes de caractères
+    # À présent on va constituer la chaîne finale
+    chaine = sep.join(parametres)
+    # On ajoute le paramètre fin à la fin de la chaîne
+    chaine += fin
+    # On affiche l'ensemble
+    print(chaine)
 
+afficher("beuleu","bordel","bite","couilles","beuleu")
