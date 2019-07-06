7e7a79a9777d8593d1976472258dcca5d85a8c43
paindespik <seandriard3@gmail.com>
2019-07-07 00:22:23 +0200

    new commit

--- untitled.py
+++ untitled.py
@@ -1,6 +1,13 @@
 #!/usr/local/lib
 # -*-coding:Utf-8 -*
 
-ma_liste=[3,7]
-ma_liste.append(56)
-print(ma_liste)
+def afficher_flottant(beuleu):
+	if type(beuleu)is not float:
+		raise TypeError("le paramètre doit être un float")
+	beuleu=str(beuleu)
+	partie_entiere, partie_flottante=beuleu.split(".")
+	beuleu2=",".join([partie_entiere,partie_flottante[:3]])
+	print(beuleu2)
+
+afficher_flottant(3.99999999)
+
